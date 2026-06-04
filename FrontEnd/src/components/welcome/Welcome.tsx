71dbfc6f682d45c019696bc8892cde706f4ef677
Sam Howard <sammy.the.howard@gmail.com>
2026-06-04 22:36:19 +0100

    WelcomePage - Updated welcome page

--- FrontEnd/src/components/welcome/Welcome.tsx
+++ FrontEnd/src/components/welcome/Welcome.tsx
@@ -5,11 +5,12 @@ export default function Welcome() {
         <div className="welcome">
             <h1>Welcome to the DnD Campaign Manager!</h1>
             <h2>This is a personal project that I've made for fun!</h2>
-            <h3>It is NOT a secure project so DO NOT use real data you use for other purposes such as passwords, usernames and email addresses</h3>
-            <h4>I have thoroughly enjoyed working on this project and I hope you enjoy using it as well!</h4> 
-            <h5>Please look at my GitHub profile for more information.</h5>
-            <h6>https://github.com/SamHowardKC/DNDCampaign</h6>
-
+            <h2>This is a work in progress, it is not complete yet, it does not function as a product as of yet.</h2>
+            <h2>It is NOT a secure project so DO NOT use real data you use for other purposes such as passwords, usernames and email addresses.</h2>
+            <h3>This project uses the free tier of an API service, therefore the API can take 30-60 seconds to spin up on first use.</h3>
+            <h3>I have thoroughly enjoyed working on this project and I hope you enjoy using it as well!</h3> 
+            <h4>Please look at my GitHub profile for more information.</h4>
+            <h4>https://github.com/SamHowardKC/DNDCampaign</h4>
             <p>Please login or register to continue.</p>
 
             <div style={{ marginTop: "20px" }}>
