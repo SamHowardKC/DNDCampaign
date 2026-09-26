2a0b3e0a1a441fef6f76af7104a5800d16c0136e
Sam Howard <sammy.the.howard@gmail.com>
2026-09-26 12:27:44 +0100

    Dashboard - Added level entity

---
+++ BackEnd/Entities/Level/Level.cs
@@ -0,0 +1,11 @@
+﻿using BackEnd.Entities;
+using Microsoft.OpenApi.Models;
+
+namespace BackEnd.Entities.Level
+{
+    public class Level : BaseEntity
+    {
+        int XPToReach;
+        int LevelNumber;
+    }
+}
